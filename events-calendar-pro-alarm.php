adbd63e53b8217794b3f8497383a9359c0d3d007
Andy Fragen <andy@thefragens.com>
2012-05-06 12:56:29 -0700

    Changed name to omit 'addon'

--- events-calendar-pro-alarm.php
+++ events-calendar-pro-alarm.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: The Events Calendar PRO Alarm Addon
+Plugin Name: The Events Calendar PRO Alarm
 Plugin URI: https://github.com/afragen/events-calendar-pro-alarm/
 Description: This plugin adds an alarm functionality to The Events Calendar PRO plugin by using the Additional Fields feature of Events Calendar PRO. This evolved from the following <a href="http://tri.be/support/forums/topic/add-alarm-to-event/">Add Alarm to Event</a> forum discussion topic. The <a href="http://tri.be/wordpress-events-calendar-pro/">Events Calendar PRO plugin</a> is required.
 Version: 1.1
@@ -12,7 +12,7 @@ License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 
 /**
-* The Events Calendar PRO Alarm Addon
+* The Events Calendar PRO Alarm
 *
 * This plugin adds an alarm functionality to The Events Calendar PRO plugin by using
 * the Additional Fields feature of Events Calendar PRO.
