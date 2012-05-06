78bda7c0cccb3a9b036f025b50d809aa2f012504
Andy Fragen <andy@thefragens.com>
2012-05-06 10:40:53 -0700

    Authorship credit to Joey Kudish

--- events-calendar-pro-alarm.php
+++ events-calendar-pro-alarm.php
@@ -4,7 +4,7 @@ Plugin Name: The Events Calendar PRO Alarm Addon
 Description: This plugin adds an alarm functionality to The Events Calendar PRO plugin by using the Additional Fields feature of Events Calendar PRO. This evolved from the following <a href="http://tri.be/support/forums/topic/add-alarm-to-event/">Add Alarm to Event</a> forum discussion topic. The <a href="http://tri.be/wordpress-events-calendar-pro/">Events Calendar PRO plugin</a> is required.
 Version: 1.0
 Text Domain: events-calendar-pro-alarm
-Author: Andy Fragen
+Author: Andy Fragen & Joey Kudish (tri.be)
 Author URI: http://thefragens.com/blog/
 License: GNU General Public License v2
 License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
