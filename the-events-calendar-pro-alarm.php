601d0b5309a51671b4064690337fafaca6521a7d
Andy Fragen <andy@thefragens.com>
2012-08-03 18:27:09 -0700

    fix typos

--- the-events-calendar-pro-alarm.php
+++ the-events-calendar-pro-alarm.php
@@ -3,7 +3,7 @@
 Plugin Name: The Events Calendar PRO Alarm
 Plugin URI: http://wordpress.org/extend/plugins/the-events-calendar-pro-alarm/
 Description: This plugin adds an alarm functionality to The Events Calendar PRO plugin by using the Additional Fields feature of Events Calendar PRO. This evolved from the following <a href="http://tri.be/support/forums/topic/add-alarm-to-event/">Add Alarm to Event</a> forum discussion topic. The <a href="http://tri.be/wordpress-events-calendar-pro/">Events Calendar PRO plugin</a> is required.
-Version: 1.1
+Version: 1.2
 Text Domain: events-calendar-pro-alarm
 Author: Andy Fragen
 Author URI: http://thefragens.com/blog/2012/05/add-alarm-to-events-calendar-pro/
@@ -46,11 +46,11 @@ License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 /* Add your functions below this line */
 
-add_action( 'plugins_loaded', 'tecpa_requires_tec' );
-function tecpa_requires_tec() {
+add_action( 'plugins_loaded', 'tecpa_requires_ecp' );
+function tecpa_requires_ecp() {
 	if ( !class_exists( 'TribeEventsPro' ) ) { 
 		echo '<div class="error">
-       <p>The Events Calendar User CSS requires The Events Calendar PRO plugin to be active.</p>
+       <p>The Events Calendar PRO Alarm requires The Events Calendar PRO plugin to be active.</p>
     </div>';
 		deactivate_plugins(__FILE__);
 	}
