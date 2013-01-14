d8e94a0c80cdb5ecdbbe8c4c2624d9fac6474d66
Andy Fragen <andy@thefragens.com>
2013-01-13 23:15:25 -0800

    remove class TribeEventsPro check

--- the-events-calendar-pro-alarm.php
+++ the-events-calendar-pro-alarm.php
@@ -14,9 +14,7 @@ License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 //Load TECProAlarm Class
 require_once( 'tecpa-class.php' );
 
-if ( class_exists( 'TribeEventsPro' ) ) {
-	add_filter( 'tribe_ical_feed_item', 'tribe_ical_add_alarm', 10, 2 );
-}
+add_filter( 'tribe_ical_feed_item', 'tribe_ical_add_alarm', 10, 2 );
 
 //This function doesn't like being in class TECProAlarm
 function tribe_ical_add_alarm( $item, $eventPost ) {
